47af9a941770d6af91bba7a89de89edf2926bc41
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-01 10:27:39 +0000

    Updated

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,3 +1,13 @@
-#!/usr/bin/env bash
-# This will create a new C# project with dotnet
-dotnet new console -o 0-new_project
+#!/bin/bash
+
+# Create a new directory titled 0-new_project
+mkdir 0-new_project
+
+# Change the current directory to 0-new_project
+cd 0-new_project
+
+# Initialize a new C# console application project using dotnet
+dotnet new console
+
+# Restore project dependencies using dotnet restore
+dotnet restore
