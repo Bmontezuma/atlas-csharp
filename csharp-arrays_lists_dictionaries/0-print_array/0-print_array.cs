2583104080fab1369971e19bc313baeef85d1e60
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 11:22:29 +0000

    Added 0-print_array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write(newArray[i] + " ");
+        }
+
+        Console.WriteLine(); // New line after printing the array
+        return newArray;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
+    }
+}
