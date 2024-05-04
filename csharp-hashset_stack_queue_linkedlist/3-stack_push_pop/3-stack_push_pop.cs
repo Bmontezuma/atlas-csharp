a13e5c43337ca5ae93319c180981120f892db7df
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 17:09:09 +0000

    Updated

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,50 +7,61 @@ public static class MyStack
     {
         Console.WriteLine("Number of items: " + aStack.Count);
 
-        if (aStack.Count > 0)
-        {
-            Console.WriteLine("Top item: " + aStack.Peek());
-        }
-        else
+        if (aStack.Count == 0)
         {
             Console.WriteLine("Stack is empty");
+            return aStack;
         }
 
         if (aStack.Contains(search))
         {
             Console.WriteLine("Stack contains " + search + ": True");
 
-            while (aStack.Count > 0 && aStack.Peek() == search)
+            Console.WriteLine("Stack after removing all items up to and including " + search + ":");
+            while (aStack.Count > 0 && aStack.Peek() != search)
             {
-                aStack.Pop();
+                Console.Write(aStack.Pop() + " ");
             }
+            Console.WriteLine(); // Print a newline after removing items
 
-            if (aStack.Count > 0)
-            {
-                Console.WriteLine("Stack after removing all items up to and including " + search + ":");
-                foreach (string item in aStack)
-                {
-                    Console.Write(item + " ");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Stack is empty after removing all items up to and including " + search);
-            }
+            aStack.Pop(); // Remove the search item
         }
         else
         {
             Console.WriteLine("Stack contains " + search + ": False");
+            return aStack;
         }
 
+        Console.WriteLine("Stack after adding " + newItem + ":");
         aStack.Push(newItem);
-
-        Console.WriteLine("\nStack after adding " + newItem + ":");
         foreach (string item in aStack)
         {
             Console.Write(item + " ");
         }
+        Console.WriteLine(); // Print a newline after printing the stack
 
         return aStack;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stack<string> stack0 = new Stack<string>(new string[] { "C", "HTML", "Javascript", "Python", "React", "Ruby" });
+        Console.WriteLine("------");
+        MyStack.Info(stack0, "C#", "Javascript");
+
+        Stack<string> stack1 = new Stack<string>(new string[] { "Physical", "Data Link", "Network", "Transport", "Session", "Presentation" });
+        Console.WriteLine("------");
+        MyStack.Info(stack1, "Application", "Application");
+
+        Stack<string> stack2 = new Stack<string>(new string[] { "C", "HTML", "Javascript", "Python", "React", "Ruby" });
+        Console.WriteLine("------");
+        MyStack.Info(stack2, "C#", "C++");
+
+        Stack<string> stack3 = new Stack<string>();
+        Console.WriteLine("------");
+        MyStack.Info(stack3, "C#", "C++");
     }
 }
