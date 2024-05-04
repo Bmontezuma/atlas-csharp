10e55de7d17dd624f8cca949bce4283f870edc77
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 17:13:44 +0000

    Updated

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,81 +1,39 @@
 using System;
 using System.Collections.Generic;
 
-public static class MyStack
+class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        Console.WriteLine("------");
-        Console.WriteLine("Number of items: " + aStack.Count);
-
-        if (aStack.Count > 0)
+        // Check if the stack is empty
+        if (aStack.Count == 0)
         {
-            Console.WriteLine("Top item: " + aStack.Peek());
-        }
-        else
-        {
+            Console.WriteLine("Number of items: 0");
             Console.WriteLine("Stack is empty");
-            return aStack; // Return early if the stack is empty
+            return aStack;
         }
 
-        bool found = false;
-        Stack<string> tempStack = new Stack<string>();
+        bool hasSearch = aStack.Contains(search);
+        Console.WriteLine($"Number of items: {aStack.Count}");
+        Console.WriteLine($"Top item: {aStack.Peek()}");
+        Console.WriteLine($"Stack contains \"{search}\": {hasSearch}");
 
-        while (aStack.Count > 0)
+        if (hasSearch)
         {
-            string item = aStack.Pop();
-            tempStack.Push(item);
-            if (item == search)
+            while (aStack.Count > 0 && aStack.Peek() != search)
             {
-                found = true;
-                break;
+                aStack.Pop();
             }
-        }
 
-        if (found)
-        {
-            Console.WriteLine("Stack contains " + search + ": True");
-            Console.WriteLine("Stack after removing all items up to and including " + search + ":");
-            foreach (string item in tempStack)
+            // Remove the search item
+            if (aStack.Count > 0)
             {
-                if (item != search)
-                {
-                    Console.Write(item + " ");
-                }
+                aStack.Pop();
             }
-            Console.WriteLine();
-        }
-        else
-        {
-            Console.WriteLine("Stack contains " + search + ": False");
         }
 
         aStack.Push(newItem);
 
-        Console.WriteLine("\nStack after adding " + newItem + ":");
-        foreach (string item in aStack)
-        {
-            Console.Write(item + " ");
-        }
-
         return aStack;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Stack<string> stack0 = new Stack<string>(new string[] { "C", "HTML", "Javascript", "Python", "React", "Ruby" });
-        MyStack.Info(stack0, "C#", "Javascript");
-
-        Stack<string> stack1 = new Stack<string>(new string[] { "Physical", "Data Link", "Network", "Transport", "Session", "Presentation" });
-        MyStack.Info(stack1, "Application", "Application");
-
-        Stack<string> stack2 = new Stack<string>(new string[] { "C", "HTML", "Javascript", "Python", "React", "Ruby" });
-        MyStack.Info(stack2, "C#", "C++");
-
-        Stack<string> stack3 = new Stack<string>();
-        MyStack.Info(stack3, "C#", "C++");
     }
 }
