cfe81f7ea4229fe1d521f5ac1381859f5190d951
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 15:46:04 +0000

    Updated

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,22 @@
+using System;
+
+public static class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int rows = myMatrix.GetLength(0);
+        int cols = myMatrix.GetLength(1);
+
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = (int)Math.Pow(myMatrix[i, j], 2);
+            }
+        }
+
+        return result;
+    }
+}
