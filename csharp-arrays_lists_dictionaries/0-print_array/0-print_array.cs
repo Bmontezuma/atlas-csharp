e886a1b79cdb950c45b208d7fac0180f1570c4ce
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 15:04:40 +0000

    Updated

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,51 +1,24 @@
 using System;
-using System.Linq;
 
-class Array
+public static class Array
 {
     public static int[] CreatePrint(int size)
     {
         if (size < 0)
         {
-            Console.WriteLine("Incorrect input");
+            Console.WriteLine("Size cannot be negative");
             return null;
         }
-        else if (size == 0)
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
         {
-            Console.WriteLine("Empty array");
-            Console.WriteLine("Array Length: 0");
-            return new int[0];
+            newArray[i] = i;
+            Console.Write(newArray[i] + " ");
         }
-        else
-        {
-            int[] nums = Enumerable.Range(1, size).ToArray();
-            Console.WriteLine("Array elements:");
-            Console.WriteLine(string.Join(" ", nums));
-            Console.WriteLine("Array Length: " + nums.Length);
-            return nums;
-        }
-    }
-}
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] newArray;
-
-        // Case: Positive size
-        Console.WriteLine("Correct output - case: positive size");
-        newArray = Array.CreatePrint(5);
-        Console.WriteLine("----------------");
-
-        // Case: Size of 0
-        Console.WriteLine("Correct output - case: size of 0");
-        newArray = Array.CreatePrint(0);
-        Console.WriteLine("----------------");
-
-        // Case: Negative size
-        Console.WriteLine("Correct output - case: negative size");
-        newArray = Array.CreatePrint(-5);
-        Console.WriteLine("----------------");
+        Console.WriteLine();
+        return newArray;
     }
 }
