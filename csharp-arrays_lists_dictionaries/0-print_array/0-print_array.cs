58e83b9e15416a9f88cd923e390278812a1fd4e3
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 14:46:47 +0000

    Updated

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -13,12 +13,39 @@ class Array
         else if (size == 0)
         {
             Console.WriteLine("Empty array");
+            Console.WriteLine("Array Length: 0");
             return new int[0];
         }
+        else
+        {
+            int[] nums = Enumerable.Range(1, size).ToArray();
+            Console.WriteLine("Array elements:");
+            Console.WriteLine(string.Join(" ", nums));
+            Console.WriteLine("Array Length: " + nums.Length);
+            return nums;
+        }
+    }
+}
 
-        int[] nums = Enumerable.Range(1, size).ToArray();
-        Console.WriteLine("Array elements:");
-        Console.WriteLine(string.Join(" ", nums));
-        return nums;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] newArray;
+
+        // Case: Positive size
+        Console.WriteLine("Correct output - case: positive size");
+        newArray = Array.CreatePrint(5);
+        Console.WriteLine("----------------");
+
+        // Case: Size of 0
+        Console.WriteLine("Correct output - case: size of 0");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("----------------");
+
+        // Case: Negative size
+        Console.WriteLine("Correct output - case: negative size");
+        newArray = Array.CreatePrint(-5);
+        Console.WriteLine("----------------");
     }
 }
