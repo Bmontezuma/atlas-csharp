120a730ac12c9a89d243f9ec98d36da081a994b6
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 17:18:10 +0000

    Updated 7

--- csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -5,18 +5,7 @@ public static class LList
 {
     public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
     {
-        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
-
-        if (myLList.Count > 0)
-        {
-            newNode.Next = myLList.First;
-            myLList.First = newNode;
-        }
-        else
-        {
-            myLList.AddFirst(newNode);
-        }
-
+        LinkedListNode<int> newNode = myLList.AddFirst(n);
         return newNode;
     }
 }
