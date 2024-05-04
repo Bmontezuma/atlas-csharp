3d6823bead8e5eaa3348ad8f12049ee9260dddc0
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 16:54:04 +0000

    Added 11-linkedlist_sum

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public static class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            sum += currentNode.Value;
+            currentNode = currentNode.Next;
+        }
+
+        return sum;
+    }
+}
