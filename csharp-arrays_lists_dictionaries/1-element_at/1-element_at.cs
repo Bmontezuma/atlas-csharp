ada4b4597efa9080364c5c8a433777af59a6236a
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 15:06:57 +0000

    Updated

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Array
+public static class Array
 {
     public static int elementAt(int[] array, int index)
     {
@@ -11,18 +11,5 @@ class Array
         }
 
         return array[index];
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        int[] array = { 10, 17, -8, 4, -12, 7, 0, 1, -1, -9 };
-
-        Console.WriteLine("Element at index {0} is {1}", 4, Array.elementAt(array, 4));
-        Console.WriteLine("Element at index {0} is {1}", 9, Array.elementAt(array, 9));
-        Console.WriteLine("Element at index {0} is {1}", 0, Array.elementAt(array, 0));
-        Array.elementAt(array, -7);
     }
 }
