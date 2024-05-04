7b3922a380937b8f6732dda38b376afb26c20466
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 09:58:39 +0000

    Added 5-print_comb

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"{i:D2}");
+            if (i < 99)
+            {
+                Console.Write(", ");
+            }
+        }
+    }
+}
