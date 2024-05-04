f440726743ffb372eadd035b8b34b3662c310044
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-04 16:48:28 +0000

    Added 8-linkedlist_find

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public static class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+
+        LinkedListNode<int> currentNode = myLList.First;
+
+        while (currentNode != null)
+        {
+            if (currentNode.Value == value)
+            {
+                return index;
+            }
+
+            currentNode = currentNode.Next;
+            index++;
+        }
+
+        return -1;
+    }
+}
