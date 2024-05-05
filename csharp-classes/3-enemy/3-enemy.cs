315228ede97bd267a5d21debae0e3a41d15637b2
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-05 18:32:42 +0000

    Added 3-enemy

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Represents a zombie enemy in the game.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>
+        /// Represents the health of the zombie.
+        /// </summary>
+        public int health;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with default health of 0.
+        /// </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class with the specified health.
+        /// </summary>
+        /// <param name="value">The initial health value.</param>
+        /// <exception cref="ArgumentException">Thrown when the provided health value is less than 0.</exception>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}
