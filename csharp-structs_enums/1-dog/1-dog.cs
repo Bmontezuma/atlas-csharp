ebbf713adc3de9738bf29fdd3a470b490d7abb2d
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-05 09:53:59 +0000

    Updated 1

--- csharp-structs_enums/1-dog/1-dog.cs
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -1,31 +1,15 @@
 using System;
 
-public enum Rating
+enum Rating
 {
     Good,
     Great,
     Excellent
 }
-
-public struct Dog
+struct Dog
 {
     public string name;
     public float age;
     public string owner;
     public Rating rating;
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dog poppy;
-
-        poppy.name = "Poppy";
-        poppy.age = 0.7f;
-        poppy.owner = "Kris";
-        poppy.rating = Rating.Excellent;
-
-        Console.WriteLine("My name is {0} and I am {1}. I am an {2} dog. :)", poppy.name, poppy.age, poppy.rating);
-    }
 }
