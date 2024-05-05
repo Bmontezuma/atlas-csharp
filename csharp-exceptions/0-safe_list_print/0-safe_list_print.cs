6ed5f56e1f757855a8a0593e4a9b1160f46eaca8
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-05 08:32:16 +0000

    Updated 0

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -1,43 +1,26 @@
-public static class List
+using System;
+using System.Collections.Generic;
+
+class List
 {
     public static int SafePrint(List<int> myList, int n)
     {
-        int count = 0;
         try
         {
-            // Check if the list is null
-            if (myList == null)
+            int count = 0;
+            foreach (int num in myList)
             {
-                throw new ArgumentNullException(nameof(myList), "List cannot be null");
-            }
-
-            // Check if the list is empty
-            if (myList.Count == 0)
-            {
-                Console.WriteLine("The list is empty");
-                return count;
-            }
-
-            // Check if n is less than 0
-            if (n < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(n), "n cannot be less than 0");
-            }
-
-            // Use Math.Min to ensure we don't go out of bounds
-            int elementsToPrint = Math.Min(n, myList.Count);
-
-            for (int i = 0; i < elementsToPrint; i++)
-            {
-                Console.Write(myList[i] + " ");
+                Console.WriteLine(num);
                 count++;
+                if (count == n)
+                    break;
             }
-            Console.WriteLine(); // Add a newline after printing
+            return count;
         }
         catch (Exception ex)
         {
             Console.WriteLine("An error occurred: " + ex.Message);
+            return 0;
         }
-        return count;
     }
 }
