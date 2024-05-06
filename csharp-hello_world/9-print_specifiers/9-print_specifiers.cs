3504389e019eda2e6de527287f5681146fa2c280
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 08:39:27 +0000

    Updated 9

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,6 @@ class Program
     {
         double percent = .7553;
         double currency = 98765.4321;
-        Console.WriteLine"(Percent: {0:P2}\nCurrency: {1:C2:}", percent, currency);
+        Console.WriteLine($"Percent: {percent:P2} tip\nCurrency: {currency:C}");
     }
 }
