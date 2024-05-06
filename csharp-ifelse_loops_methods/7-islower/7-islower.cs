2dd0571fcbfa6f44eb7a4b889ea58e7363a54c59
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 09:35:50 +0000

    Updated 7

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,11 +1,25 @@
 using System;
 
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        return c >= 'a' && c <= 'z';
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(Character.IsLower('a')); // Output: True
-        Console.WriteLine(Character.IsLower('A')); // Output: False
-        Console.WriteLine(Character.IsLower('1')); // Output: False
+        char[] letters = { 'a', 'A', 'Q', 'h', '9', 'B', 'g'};
+        
+        foreach (char letter in letters)
+        {
+            if (Character.IsLower(letter))
+                Console.WriteLine("{0} is lowercase", letter);
+            else
+                Console.WriteLine("{0} is uppercase", letter);
+        }
     }
 }
