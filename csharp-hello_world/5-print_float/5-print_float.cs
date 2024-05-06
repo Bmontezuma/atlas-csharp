46ef9f72b7265b1534f1b3a72d7d20ddfb3fab34
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 08:36:20 +0000

    Updated 5

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,10 +1,10 @@
 using System;
+
 class Program
 {
-	static void Main(string[] args)
-        {
-		float number = 3.1i4159f;
-		float roundedNumber = (float)Math.Round(number, 2);
-		Console.WriteLine(roundedNumber);
-        }
+    static void Main(string[] args)
+    {
+        float number = 3.14159f;
+        Console.WriteLine($"Float: {number:F2}");
+    }
 }
