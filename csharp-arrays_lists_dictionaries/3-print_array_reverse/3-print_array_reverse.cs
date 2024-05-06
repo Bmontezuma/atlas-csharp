73070f89b279062b78ca5622b60afb6845cc2b74
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 09:48:57 +0000

    Updated 3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,14 +1,18 @@
-using System;
-
 public static class Array
 {
     public static void Reverse(int[] array)
     {
+        if (array == null)
+        {
+            Console.WriteLine("Array cannot be null");
+            return;
+        }
+
         int length = array.Length;
 
         for (int i = length - 1; i >= 0; i--)
         {
-            Console.Write(array[i] + " ");
+            Console.Write($"{array[i]} ");
         }
 
         Console.WriteLine();
