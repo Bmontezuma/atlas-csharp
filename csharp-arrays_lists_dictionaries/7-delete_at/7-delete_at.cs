0e06cc74ebf78287c8ab34b40657ca72fd17533f
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 10:06:55 +0000

    Updated

--- csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 
 class ListHelper {
-    public static void DeleteAt(List<int> myList, int index) {
-        if (index < 0 || index > myList.Count - 1) {
+    public static List<int> DeleteAt(List<int> myList, int index) {
+        if (index < 0 || index >= myList.Count) {
             Console.WriteLine("Index is out of range");
-            return;
+            return myList;
         }
-        myList.RemoveAt(index);
+        
+        List<int> updatedList = new List<int>(myList.Count - 1);
+        for (int i = 0; i < myList.Count; i++) {
+            if (i != index) {
+                updatedList.Add(myList[i]);
+            }
+        }
+        
+        return updatedList;
     }
 }
