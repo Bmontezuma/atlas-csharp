80accf57ade3f0eed58bc280304ed97991c691ff
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-06 09:29:02 +0000

    Updated 4

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -1,17 +1,12 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        for (int i = 0, j = 0x10; i < 100; i++, j += 0x10)
+        static void Main(string[] args)
         {
-            Console.Write($"{i} = 0x{i:X2} ");
-            if (i == j - 0x10)
-            {
-                Console.WriteLine();
-                Console.Write($"{i + 1} = 0x{i + 1:X2} ");
-            }
+           for (int i = 0; i <= 98; i++) {
+               Console.Write("{0} = 0x{0:x}\n", i);
+           }
         }
     }
-}
+
