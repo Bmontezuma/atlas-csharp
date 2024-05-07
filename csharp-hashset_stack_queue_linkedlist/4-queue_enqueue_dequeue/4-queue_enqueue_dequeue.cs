e906ac4e93f6c8825de62a14dde41f8fb8a9b1e9
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-07 09:46:39 +0000

    Updated

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -5,36 +5,52 @@ class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        bool dequeueUsed = false;
-
+        // Print the number of items in the queue
         Console.WriteLine($"Number of items: {aQueue.Count}");
 
+        // Print the item at the top of the queue without removing it
         if (aQueue.Count > 0)
-        {
             Console.WriteLine($"First item: {aQueue.Peek()}");
-        }
         else
-        {
             Console.WriteLine("Queue is empty");
-        }
 
+        // Add a new item to the queue
         aQueue.Enqueue(newItem);
 
-        Console.WriteLine($"Queue contains {search}: {aQueue.Contains(search)}");
+        // Print if the queue contains the given item
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
 
-        if (aQueue.Contains(search))
-        {
-            while (aQueue.Peek() != search && !dequeueUsed)
-            {
-                aQueue.Dequeue();
-                dequeueUsed = true;
-            }
-            if (dequeueUsed)
-            {
-                aQueue.Dequeue();
-            }
-        }
+        // Remove items up to and including the given item, if it exists
+        while (aQueue.Count > 0 && aQueue.Peek() != search)
+            aQueue.Dequeue();
 
         return aQueue;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> aQueue = new Queue<string>();
+
+        aQueue.Enqueue("C");
+        aQueue.Enqueue("HTML");
+        aQueue.Enqueue("Javascript");
+        aQueue.Enqueue("Python");
+        aQueue.Enqueue("React");
+        aQueue.Enqueue("Ruby");
+
+        foreach (string item in aQueue)
+            Console.WriteLine(item);
+
+        Console.WriteLine("------");
+
+        MyQueue.Info(aQueue, "C#", "Javascript");
+
+        Console.WriteLine("------");
+
+        foreach (string item in aQueue)
+            Console.WriteLine(item);
     }
 }
