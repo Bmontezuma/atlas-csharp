3dfb969a610f30fcd5d3bd625e404c65bc3420a1
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-07 09:36:45 +0000

    Updated

--- csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -2,21 +2,16 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-class List 
-{
-    public static List<int> CreatePrint(int size, int printCount = 1)
+class List {
+    public static List<int> CreatePrint(int size)
     {
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return null;
         }
-        List<int> newList = Enumerable.Range(0, size).ToList();
-        Console.WriteLine(string.Join(" ", newList));
-        for (int i = 0; i < printCount; i++)
-        {
-            Console.WriteLine($"List Length: {newList.Count}");
-        }
-        return newList;
+        var ls = Enumerable.Range(0, size).ToList();
+        Console.WriteLine("{0}", string.Join(" ", ls));
+        return ls;
     }
 }
