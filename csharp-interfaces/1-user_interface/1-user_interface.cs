ab534a088abf067a8480b0206694d4c83336c108
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 18:16:04 +0000

    Updated

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+/// <summary>
+/// An interface for interactive objects.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Method for interacting with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// An abstract class representing a base entity.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Returns a string representation of the entity.
+    /// </summary>
+    /// <returns>A string in the format '<see cref="name"/> is a <see cref="T:Base"/>'.</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>
+/// Represents a door object.
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Constructor for the Door class.
+    /// </summary>
+    /// <param name="name">The name of the door (optional).</param>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    /// <summary>
+    /// Interaction method for the door.
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
