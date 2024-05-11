6ebd3bfc9e8087cda20cd818201fea52d66eeb74
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 13:53:07 +0000

    Updated

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Obj
+{
+    /// <summary>
+    /// Checks if the object is an instance of, or inherits from, Array.
+    /// </summary>
+    /// <param name="obj">The object to check.</param>
+    /// <returns>True if the object is an instance of Array or inherits from Array, otherwise False.</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
