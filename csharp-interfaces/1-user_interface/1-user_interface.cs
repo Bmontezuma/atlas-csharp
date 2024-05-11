fa52cd906c9638cab93835714b6fd3feb4cd225f
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 18:47:40 +0000

    Updated

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -12,26 +12,6 @@ public interface IInteractive
 }
 
 /// <summary>
-/// An abstract class representing a base entity.
-/// </summary>
-public abstract class Base
-{
-    /// <summary>
-    /// Gets or sets the name of the entity.
-    /// </summary>
-    public string name { get; set; }
-
-    /// <summary>
-    /// Returns a string representation of the entity.
-    /// </summary>
-    /// <returns>A string in the format '<see cref="name"/> is a <see cref="T:Base"/>'.</returns>
-    public override string ToString()
-    {
-        return $"{name} is a {GetType().Name}";
-    }
-}
-
-/// <summary>
 /// Represents a door object.
 /// </summary>
 public class Door : Base, IInteractive
@@ -39,7 +19,7 @@ public class Door : Base, IInteractive
     /// <summary>
     /// Constructor for the Door class.
     /// </summary>
-    /// <param name="name">The name of the door (optional).</param>
+    /// <param name="name">The name of the door.</param>
     public Door(string name = "Door")
     {
         this.name = name;
