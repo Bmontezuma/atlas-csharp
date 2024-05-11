eb6a6eddab4613bf47d3c5a2f03d180617731165
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 20:14:53 +0000

    Updated

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,72 +1,21 @@
-﻿namespace Claptrap
+﻿using System;
+
+/// <summary>
+/// An abstract class representing a base entity.
+/// </summary>
+public abstract class Base
 {
     /// <summary>
-    /// Base class for objects to inherit.
+    /// Gets or sets the name of the entity.
     /// </summary>
-    public abstract class Base
-    {
-        /// <summary>
-        /// Public name of the object.
-        /// </summary>
-        public string name;
-
-        /// <summary>
-        /// Returns a message.
-        /// </summary>
-        /// <returns>string</returns>
-        public override string ToString()
-        {
-            return string.Format("{0} is a {1}", name, this.GetType().Name);
-        }
-    }
+    public string Name { get; set; }
 
     /// <summary>
-    /// Makes an object interactable.
+    /// Returns a string representation of the entity.
     /// </summary>
-    interface IInteractive
+    /// <returns>A string in the format '<see cref="Name"/> is a <see cref="T:Base"/>'.</returns>
+    public override string ToString()
     {
-        void Interact();
-    }
-
-    /// <summary>
-    /// Gives objects a durability property and breakable state.
-    /// </summary>
-    interface IBreakable
-    {
-        int durability { get; set; }
-        void Break();
-    }
-
-    /// <summary>
-    /// Makes an object collectible.
-    /// </summary>
-    interface ICollectable
-    {
-        bool isCollected { get; set; }
-        void Collect();
-    }
-
-    /// <summary>
-    /// Represents a test object inheriting from Base and implementing interfaces.
-    /// </summary>
-    class TestObject : Base, IInteractive, IBreakable, ICollectable
-    {
-        public int durability { get; set; }
-        public bool isCollected { get; set; }
-
-        public void Interact()
-        {
-            // Implementation of interaction behavior.
-        }
-
-        public void Break()
-        {
-            // Implementation of break behavior.
-        }
-
-        public void Collect()
-        {
-            // Implementation of collect behavior.
-        }
+        return $"{Name} is a {GetType().Name}";
     }
 }
