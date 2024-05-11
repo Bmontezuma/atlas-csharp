fcddaeaded1386ccadfa83a084bda72e672dbf83
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 17:10:07 +0000

    Updated

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace csharp_interfaces
+{
+    /// <summary>
+    /// Abstract base class for all objects.
+    /// </summary>
+    public abstract class Base
+    {
+        /// <summary>
+        /// The name of the object.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Returns a string representation of the object.
+        /// </summary>
+        /// <returns>A string representation of the object.</returns>
+        public override string ToString()
+        {
+            return $"{name} is a {this.GetType().Name}";
+        }
+    }
+
+    /// <summary>
+    /// A software engineer.
+    /// </summary>
+    public class SoftwareEngineer : Base
+    {
+        // Empty class
+    }
+}
