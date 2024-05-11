761055ee3f61b352d17b5afb0fb0c01a6aa4aa3a
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 12:35:23 +0000

    Updated 1 MyMath.cs

--- csharp-tdd/1-divide/MyMath/MyMath.cs
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -1,13 +1,23 @@
-﻿namespace MyMath
+﻿using System;
+
+namespace MyMath
 {
     public static class Matrix
     {
+        /// <summary>
+        /// Divides all elements of a matrix by a specified number.
+        /// </summary>
+        /// <param name="matrix">The matrix to divide.</param>
+        /// <param name="num">The number to divide each element by.</param>
+        /// <returns>A new matrix containing the divided elements.</returns>
+        /// <remarks>
+        /// If num is 0, prints "Num cannot be 0" and returns null.
+        /// If matrix is null, returns null.
+        /// </remarks>
         public static int[,] Divide(int[,] matrix, int num)
         {
             if (matrix == null)
-            {
                 return null;
-            }
 
             if (num == 0)
             {
@@ -15,27 +25,19 @@
                 return null;
             }
 
-            try
-            {
-                int rows = matrix.GetLength(0);
-                int cols = matrix.GetLength(1);
-                int[,] result = new int[rows, cols];
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            int[,] result = new int[rows, cols];
 
-                for (int i = 0; i < rows; i++)
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
                 {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        result[i, j] = matrix[i, j] / num;
-                    }
+                    result[i, j] = matrix[i, j] / num;
                 }
+            }
 
-                return result;
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Cannot divide by zero");
-                return null;
-            }
+            return result;
         }
     }
 }
