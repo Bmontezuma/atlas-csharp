67397fe9b5ad7e40ddf25384aff5030d18d2d2d7
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 12:24:56 +0000

    Udpated

--- csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -5,49 +5,65 @@ namespace MyMath.Tests
 {
     public class MatrixTests
     {
+        /// <summary>
+        /// Tests the Divide method with a valid matrix.
+        /// </summary>
         [Test]
-        public void Divide_ValidMatrixAndNum_ReturnsDividedMatrix()
+        public void Divide_ValidMatrix_ReturnsDividedMatrix()
         {
-            int[,] matrix = new int[,] { { 1, 2 }, { 3, 4 } };
+            int[,] matrix = new int[,] { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
             int num = 2;
-            int[,] expected = new int[,] { { 0, 1 }, { 1, 2 } };
+            int[,] expected = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
             int[,] result = Matrix.Divide(matrix, num);
 
             Assert.AreEqual(expected, result);
         }
 
+        /// <summary>
+        /// Tests the Divide method with a matrix that contains a 0.
+        /// </summary>
+        [Test]
+        public void Divide_MatrixWithZero_ReturnsNull()
+        {
+            int[,] matrix = new int[,] { { 2, 4, 6 }, { 8, 0, 12 }, { 14, 16, 18 } };
+            int num = 2;
+            int[,] expected = null;
+
+            int[,] result = Matrix.Divide(matrix, num);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        /// <summary>
+        /// Tests the Divide method with a null matrix.
+        /// </summary>
         [Test]
         public void Divide_NullMatrix_ReturnsNull()
         {
             int[,] matrix = null;
             int num = 2;
+            int[,] expected = null;
 
             int[,] result = Matrix.Divide(matrix, num);
 
-            Assert.IsNull(result);
+            Assert.AreEqual(expected, result);
         }
 
+        /// <summary>
+        /// Tests the Divide method with a 0 divisor.
+        /// </summary>
         [Test]
-        public void Divide_ZeroNum_ReturnsNull()
+        public void Divide_ZeroDivisor_PrintsErrorMessage()
         {
-            int[,] matrix = new int[,] { { 1, 2 }, { 3, 4 } };
+            int[,] matrix = new int[,] { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
             int num = 0;
+            int[,] expected = null;
 
             int[,] result = Matrix.Divide(matrix, num);
 
-            Assert.IsNull(result);
-        }
-
-        [Test]
-        public void Divide_DivideByZero_ReturnsNull()
-        {
-            int[,] matrix = new int[,] { { 1, 0 }, { 3, 4 } };
-            int num = 0;
-
-            int[,] result = Matrix.Divide(matrix, num);
-
-            Assert.IsNull(result);
+            Assert.AreEqual(expected, result);
+            Assert.AreEqual("Num cannot be 0", Console.LastLineWritten);
         }
     }
 }
