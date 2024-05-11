54f24d0c7509112e5e7e465250b8fce870ee32da
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 12:15:40 +0000

    Updated 2-max_int with XML

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,6 +1,25 @@
-﻿namespace MyMath;
-
-public class Class1
+﻿namespace MyMath
 {
+    public static class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            if (nums == null || nums.Count == 0)
+            {
+                return 0;
+            }
 
+            int max = nums[0];
+
+            foreach (int num in nums)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            return max;
+        }
+    }
 }
