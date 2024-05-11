31887147461a3f2f6ba71329b6e89f172582ce3c
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 14:10:19 +0000

    Updated 2-subclass

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Obj
+{
+    /// <summary>
+    /// Checks if the specified type is a subclass of the specified base type.
+    /// </summary>
+    /// <param name="derivedType">The derived type to check.</param>
+    /// <param name="baseType">The base type to check against.</param>
+    /// <returns>True if the derived type is a subclass of the base type and not the same as the base type, otherwise False.</returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return derivedType.IsSubclassOf(baseType) && derivedType != baseType;
+    }
+}
