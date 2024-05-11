c9a891a730c2ea751612ec4f0fb3eea4952a4d07
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 14:17:11 +0000

    Updated 3-type_get

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+public class Obj
+{
+    /// <summary>
+    /// Prints the names of the available properties and methods of an object.
+    /// </summary>
+    /// <param name="myObj">The object to inspect.</param>
+    public static void Print(object myObj)
+    {
+        Type type = myObj.GetType();
+
+        Console.WriteLine($"{type.Name} Properties:");
+        foreach (var property in type.GetProperties())
+        {
+            Console.WriteLine(property.Name);
+        }
+
+        Console.WriteLine($"{type.Name} Methods:");
+        foreach (var method in type.GetMethods())
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
