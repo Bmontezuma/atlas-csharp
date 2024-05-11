d592813e1ae10889ed1ee4807aa075c03a23506f
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 13:16:10 +0000

    Files for 0-is

---
+++ csharp-inheritance/0-is/0-is/Obj.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Obj
+{
+    public static class Obj
+    {
+        /// <summary>
+        /// Returns True if the object is an int, otherwise return False.
+        /// </summary>
+        /// <param name="obj">The object to check.</param>
+        /// <returns>True if the object is an int, otherwise False.</returns>
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj is int;
+        }
+
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = new List<int>();
+
+            if (IsOfTypeInt(a))
+                Console.WriteLine("{0} is of type int", nameof(a));
+            else
+                Console.WriteLine("{0} is NOT of type int", nameof(a));
+
+            if (IsOfTypeInt(b))
+                Console.WriteLine("{0} is of type int", nameof(b));
+            else
+                Console.WriteLine("{0} is NOT of type int", nameof(b));
+        }
+    }
+}
