6c409b958946dbd53983715d7726616a5644cefb
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 19:57:59 +0000

    Updated

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 
-/// <summary>
-/// An abstract class representing a base entity.
-/// </summary>
-public abstract class Base
+namespace Borderlands3.Entities
 {
     /// <summary>
-    /// Gets or sets the name of the entity.
+    /// An abstract class representing a base entity in Borderlands 3.
     /// </summary>
-    public string name { get; set; }
+    public abstract class Base
+    {
+        /// <summary>
+        /// Gets or sets the name of the entity.
+        /// </summary>
+        public string name { get; set; }
+
+        /// <summary>
+        /// Returns a string representation of the entity.
+        /// </summary>
+        /// <returns>A string in the format '<see cref="name"/> is a <see cref="T:Base"/>'.</returns>
+        public override string ToString()
+        {
+            return $"{name} is a {GetType().Name}";
+        }
+    }
 
     /// <summary>
-    /// Returns a string representation of the entity.
+    /// A class representing Claptrap in Borderlands 3.
     /// </summary>
-    /// <returns>A string in the format '<see cref="name"/> is a <see cref="T:Base"/>'.</returns>
-    public override string ToString()
+    public class Claptrap : Base
     {
-        return $"{name} is a {GetType().Name}";
+        // Empty class
     }
 }
