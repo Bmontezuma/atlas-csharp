286d7e3040c365ad4e31d2cd6e494a4f967cd43e
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 18:57:17 +0000

    Updated

--- csharp-interfaces/1-user_interface/1-user_interface.cs
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,6 +1,26 @@
 ﻿using System;
 
 /// <summary>
+/// An abstract class representing a base entity.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Returns a string representation of the entity.
+    /// </summary>
+    /// <returns>A string in the format '<see cref="name"/> is a <see cref="T:Base"/>'.</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType().Name}";
+    }
+}
+
+/// <summary>
 /// An interface for interactive objects.
 /// </summary>
 public interface IInteractive
