252aae5c95a60f53eefc9d5575702d70ca1acddf
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 16:08:15 +0000

    Updated

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,7 +1,14 @@
 ﻿using System;
 
+/// <summary>
+/// Represents a basic shape.
+/// </summary>
 public class Shape
 {
+    /// <summary>
+    /// Calculates the area of the shape.
+    /// </summary>
+    /// <returns>The area of the shape.</returns>
     public virtual int Area()
     {
         throw new NotImplementedException("Area() is not implemented");
