e914d3bee57131ca08514a8b24c09ab524fbd702
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-11 00:22:12 +0000

    Lets try this 0-add

--- csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,15 +1,45 @@
-namespace MyMath.Tests;
+using System;
+using NUnit.Framework;
+using MyMath;
 
-public class Tests
+namespace MyMath.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class OperationsTests
     {
-    }
+        [Test]
+        public void Add_TwoIntegers_ReturnsSum()
+        {
+            int a = 3;
+            int b = 5;
+            int expected = 8;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            int result = Operations.Add(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_ZeroAndZero_ReturnsZero()
+        {
+            int a = 0;
+            int b = 0;
+            int expected = 0;
+
+            int result = Operations.Add(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Add_NegativeAndPositive_ReturnsDifference()
+        {
+            int a = -3;
+            int b = 5;
+            int expected = 2;
+
+            int result = Operations.Add(a, b);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
--- csharp-tdd/0-add/MyMath/MyMath.cs
+++ csharp-tdd/0-add/MyMath/MyMath.cs
@@ -1,6 +1,18 @@
-﻿namespace MyMath;
+﻿using System;
 
-public class Class1
+namespace MyMath
 {
-
+    public static class Operations
+    {
+        /// <summary>
+        /// Adds two integers.
+        /// </summary>
+        /// <param name="a">The first integer.</param>
+        /// <param name="b">The second integer.</param>
+        /// <returns>The sum of the two integers.</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
 }
