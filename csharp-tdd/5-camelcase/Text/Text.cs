e906aea70812b36b3d5e4d0de41395256b8e976c
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 15:03:00 +0000

    Updated

--- csharp-tdd/5-camelcase/Text/Text.cs
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -1,6 +1,29 @@
-﻿namespace Text;
+﻿using System;
 
-public class Class1
+namespace Text
 {
+    /// <summary>
+    /// Class containing string manipulation methods.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Determines the number of words in a camelCase string.
+        /// </summary>
+        /// <param name="s">Input string.</param>
+        /// <returns>The number of words in the string.</returns>
+        public static int CamelCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
 
+            int wordCount = 1;
+            foreach (char c in s)
+            {
+                if (char.IsUpper(c))
+                    wordCount++;
+            }
+            return wordCount;
+        }
+    }
 }
