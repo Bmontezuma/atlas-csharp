501e8757ada440f725bcc7afd878a19c9378bd26
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 13:34:09 +0000

    Updated

--- csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,25 +1,26 @@
-﻿namespace MyMath
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
 {
-    public static class Operations
+    /// <summary>
+    /// Class containing mathematical operations.
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// Returns the maximum integer in a list of integers.
+        /// </summary>
+        /// <param name="nums">List of integers.</param>
+        /// <returns>The maximum integer in the list. Returns 0 if the list is empty.</returns>
         public static int Max(List<int> nums)
         {
+            // If the list is null or empty, return 0
             if (nums == null || nums.Count == 0)
-            {
                 return 0;
-            }
 
-            int max = nums[0];
-
-            foreach (int num in nums)
-            {
-                if (num > max)
-                {
-                    max = num;
-                }
-            }
-
-            return max;
+            // Otherwise, return the maximum value in the list
+            return nums.Max();
         }
     }
 }
