903fff02b44612415c2d6c6def4c2c415d81d0e7
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 21:58:13 +0000

    Updated

--- csharp-inheritance/6-shape/main.cs
+++ csharp-inheritance/6-shape/main.cs
@@ -1,28 +1,31 @@
 using System;
 
-class Program
+namespace ShapeProgram
 {
-    static void Main(string[] args)
+    class Program
     {
-        Rectangle aRect = new Rectangle();
-        aRect.Width = 3;
-        aRect.Height = 8;
-
-        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
-            Console.WriteLine("Rectangle is a subclass of Shape");
-        else
-            Console.WriteLine("Rectangle is NOT a subclass of Shape");
-
-        Console.WriteLine("Width: {0}", aRect.Width);
-        Console.WriteLine("Height: {0}", aRect.Height);
-
-        try
+        static void Main(string[] args)
         {
-            Console.WriteLine("Area: {0}", aRect.Area());
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
+            Rectangle aRect = new Rectangle();
+            aRect.Width = 3;
+            aRect.Height = 8;
+
+            if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
+                Console.WriteLine("Rectangle is a subclass of Shape");
+            else
+                Console.WriteLine("Rectangle is NOT a subclass of Shape");
+
+            Console.WriteLine("Width: {0}", aRect.Width);
+            Console.WriteLine("Height: {0}", aRect.Height);
+
+            try
+            {
+                Console.WriteLine("Area: {0}", aRect.Area());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
