a3ca04db841928fb0482ed0e97788803c6d5a22f
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 11:02:42 +0000

    Updated

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -1,73 +1,11 @@
 ﻿using System;
 
-namespace Base
+abstract class Base
 {
-    /// <summary>
-    /// Represents a base entity with a name.
-    /// </summary>
-    public abstract class Base
+    public string name { get; set; }
+
+    public override string ToString()
     {
-        /// <summary>
-        /// Gets or sets the name of the entity.
-        /// </summary>
-        public string Name { get; set; }
-
-        /// <summary>
-        /// Returns a string representation of the entity.
-        /// </summary>
-        /// <returns>A string representation of the entity.</returns>
-        public override string ToString()
-        {
-            if (!string.IsNullOrEmpty(Name))
-                return $"{Name} is a {GetType().Name}";
-            else
-                return $"{GetType().Name} (no name set)";
-        }
-    }
-
-    /// <summary>
-    /// Represents a software engineer entity.
-    /// </summary>
-    public class SoftwareEngineer : Base
-    {
-        /// <summary>
-        /// Gets or sets the programming language the software engineer works with.
-        /// </summary>
-        public string ProgrammingLanguage { get; set; }
-
-        /// <summary>
-        /// Simulates the software engineer writing code.
-        /// </summary>
-        public void WriteCode()
-        {
-            Console.WriteLine($"{Name} is writing code in {ProgrammingLanguage}.");
-        }
-    }
-
-    /// <summary>
-    /// Represents a test object entity.
-    /// </summary>
-    public class TestObject : Base
-    {
-        /// <summary>
-        /// Gets or sets a value indicating whether the test object is valid.
-        /// </summary>
-        public bool IsValid { get; set; }
-
-        /// <summary>
-        /// Runs tests on the test object.
-        /// </summary>
-        public void RunTests()
-        {
-            Console.WriteLine($"Running tests on {Name}. Result: {(IsValid ? "Pass" : "Fail")}");
-        }
-    }
-
-    class Program
-    {
-        static void Main()
-        {
-
-        }
+        return $"{name} is a {this.GetType().Name}";
     }
 }
