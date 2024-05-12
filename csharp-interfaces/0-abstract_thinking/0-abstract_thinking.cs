e47b9b6e8d05860bdfcfffb7fde59fa91ab72b66
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 11:09:07 +0000

    Updated

--- csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -12,6 +12,41 @@ public abstract class Base
 
     public override string ToString()
     {
-        return $"{Name} is a {GetType().Name}";
+        if (!string.IsNullOrEmpty(Name))
+            return $"{Name} is a {GetType().Name}";
+        else
+            return $"{GetType().Name} (no name set)";
+    }
+}
+
+public class SoftwareEngineer : Base
+{
+    public SoftwareEngineer()
+    {
+        Name = "Betty"; // Setting the Name property
+    }
+}
+
+public class TestObject : Base
+{
+    public TestObject()
+    {
+        Name = "Abstract Class"; // Setting the Name property
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var engineer = new SoftwareEngineer();
+        Console.WriteLine(engineer);
+
+        var testObjectWithName = new TestObject();
+        Console.WriteLine(testObjectWithName);
+
+        var testObjectWithoutName = new TestObject();
+        testObjectWithoutName.Name = null;
+        Console.WriteLine(testObjectWithoutName);
     }
 }
