c8494519d225990daa7802a283342f4b324bcc64
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 14:54:44 +0000

    Updated

--- csharp-tdd/4-unique/Text/Text.cs
+++ csharp-tdd/4-unique/Text/Text.cs
@@ -1,6 +1,45 @@
-﻿namespace Text;
+﻿using System.Collections.Generic;
 
-public class Class1
+namespace Text
 {
+    /// <summary>
+    /// Class containing string manipulation methods.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Returns the index of the first non-repeating character in a string.
+        /// </summary>
+        /// <param name="s">Input string.</param>
+        /// <returns>Index of the first non-repeating character, or -1 if there is none.</returns>
+        public static int UniqueChar(string s)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
 
+            // Count occurrences of each character
+            foreach (char c in s)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            // Find the first character with count 1
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charCount[s[i]] == 1)
+                {
+                    return i;
+                }
+            }
+
+            // No non-repeating character found
+            return -1;
+        }
+    }
 }
