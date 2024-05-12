207646dde421f546ae9df533a24af3c4d60ce0cb
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 22:13:10 +0000

    Updated

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -79,30 +79,3 @@ public class Rectangle : Shape
         return Width * Height;
     }
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Rectangle aRect = new Rectangle();
-        aRect.Width = 3;
-        aRect.Height = 8;
-
-        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
-            Console.WriteLine("Rectangle is a subclass of Shape");
-        else
-            Console.WriteLine("Rectangle is NOT a subclass of Shape");
-
-        Console.WriteLine("Width: {0}", aRect.Width);
-        Console.WriteLine("Height: {0}", aRect.Height);
-
-        try
-        {
-            Console.WriteLine("Area: {0}", aRect.Area());
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
-    }
-}
