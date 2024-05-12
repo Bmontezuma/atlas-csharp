31e781338d65052e96b2d4e7d5ea9a08fdda7318
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 11:31:32 +0000

    Updated

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,67 @@
+using System;
+
+/// <summary>
+/// Base class representing a generic entity.
+/// </summary>
+abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name of the entity.
+    /// </summary>
+    public string name { get; set; }
+
+    /// <summary>
+    /// Override of the ToString() method to provide a string representation of the entity.
+    /// </summary>
+    /// <returns>A string representation of the entity.</returns>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType()}";
+    }
+}
+
+/// <summary>
+/// Interface for interactive objects.
+/// </summary>
+interface IInteractive
+{
+    /// <summary>
+    /// Interact with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// Represents a door entity.
+/// </summary>
+class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Constructor for the Door class.
+    /// </summary>
+    /// <param name="name">The name of the door.</param>
+    public Door(string name)
+    {
+        this.name = string.IsNullOrEmpty(name) ? "Door" : name;
+    }
+
+    /// <summary>
+    /// Implementation of the Interact method for the Door class.
+    /// </summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {this.name}. It's locked.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Door frontDoor = new Door("Front Door");
+
+        Console.WriteLine(frontDoor.ToString());
+
+        frontDoor.Interact();
+    }
+}
