b1487b1fc640cf2dd50fd2d7cc1c1d3f47d0a1c3
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 16:35:12 +0000

    Updated

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,91 +1,38 @@
 ﻿using System;
 
-/// <summary>
-/// Represents a geometric shape.
-/// </summary>
-public class Shape
-{
-    /// <summary>
-    /// Calculates the area of the shape.
-    /// </summary>
-    /// <returns>The area of the shape.</returns>
-    public virtual double Area()
-    {
-        throw new NotImplementedException("Area() is not implemented");
-    }
-}
-
-/// <summary>
-/// Represents a rectangle, a type of geometric shape.
-/// </summary>
-public class Rectangle : Shape
+class Rectangle : Shape
 {
     private int width;
     private int height;
 
-    /// <summary>
-    /// Gets or sets the width of the rectangle.
-    /// </summary>
-    /// <exception cref="ArgumentException">Thrown when the width is set to a negative value.</exception>
     public int Width
     {
         get { return width; }
         set
         {
             if (value < 0)
-                throw new ArgumentException("Width must be greater than or equal to 0");
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0.");
+            }
             width = value;
         }
     }
 
-    /// <summary>
-    /// Gets or sets the height of the rectangle.
-    /// </summary>
-    /// <exception cref="ArgumentException">Thrown when the height is set to a negative value.</exception>
     public int Height
     {
         get { return height; }
         set
         {
             if (value < 0)
-                throw new ArgumentException("Height must be greater than or equal to 0");
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0.");
+            }
             height = value;
         }
     }
 
-    /// <summary>
-    /// Calculates the area of the rectangle.
-    /// </summary>
-    /// <returns>The area of the rectangle.</returns>
-    public override double Area()
+    public override int Area()
     {
         return width * height;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Rectangle aRect = new Rectangle();
-        aRect.Width = 3;
-        aRect.Height = 8;
-
-        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
-            Console.WriteLine("Rectangle is a subclass of Shape");
-        else
-            Console.WriteLine("Rectangle is NOT a subclass of Shape");
-
-        Console.WriteLine("Width: {0}", aRect.Width);
-        Console.WriteLine("Height: {0}", aRect.Height);
-
-        try
-        {
-            Console.WriteLine("Area: {0}", aRect.Area());
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-        }
     }
 }
