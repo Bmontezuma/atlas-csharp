82306455c899665f068daf7aa88e143f4db90337
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-12 14:25:01 +0000

    Updated

--- csharp-tdd/3-palindrome/Text/Text.cs
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -1,6 +1,25 @@
-﻿namespace Text;
+﻿using System;
+using System.Linq;
 
-public class Class1
+namespace Text
 {
+    /// <summary>
+    /// Class containing string manipulation methods.
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Checks if a string is a palindrome.
+        /// </summary>
+        /// <param name="s">Input string.</param>
+        /// <returns>True if string is a palindrome, False otherwise.</returns>
+        public static bool IsPalindrome(string s)
+        {
+            // Remove spaces and punctuation, convert to lowercase
+            string cleanedString = new string(s.Where(char.IsLetterOrDigit).ToArray()).ToLower();
 
+            // Compare the cleaned string with its reverse
+            return cleanedString.SequenceEqual(cleanedString.Reverse());
+        }
+    }
 }
