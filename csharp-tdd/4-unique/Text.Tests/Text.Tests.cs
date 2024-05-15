bda5701b2f6204f848668a3df26373d56b3bdfb1
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-15 18:54:39 -0500

    Updated

--- csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -3,72 +3,71 @@ using Text;
 
 namespace Text.Tests
 {
-    [TestFixture]
     public class StrTests
     {
         [Test]
-        public void RegularPalindrome_ReturnsTrue()
+        public void UniqueChar_FirstUniqueIsFirstChar_ReturnsZero()
         {
             // Arrange
-            string input = "Racecar";
+            string input = "helloworld";
 
             // Act
-            bool result = Str.IsPalindrome(input);
+            int result = Str.UniqueChar(input);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.AreEqual(0, result);
         }
 
         [Test]
-        public void UpperLowerCaseLetters_ReturnsTrue()
+        public void UniqueChar_FirstUniqueIsLastChar_ReturnsCorrectIndex()
         {
             // Arrange
-            string input = "LeVel";
+            string input = "helloholberton";
 
             // Act
-            bool result = Str.IsPalindrome(input);
+            int result = Str.UniqueChar(input);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.AreEqual(8, result);
         }
 
         [Test]
-        public void Punctuation_ReturnsTrue()
+        public void UniqueChar_FirstUniqueInMiddle_ReturnsCorrectIndex()
         {
             // Arrange
-            string input = "A man, a plan, a canal: Panama.";
+            string input = "aabbccddeef";
 
             // Act
-            bool result = Str.IsPalindrome(input);
+            int result = Str.UniqueChar(input);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.AreEqual(10, result);
         }
 
         [Test]
-        public void Spaces_ReturnsTrue()
+        public void UniqueChar_NoUniqueCharacters_ReturnsMinusOne()
         {
             // Arrange
-            string input = "taco cat";
+            string input = "hellohello";
 
             // Act
-            bool result = Str.IsPalindrome(input);
+            int result = Str.UniqueChar(input);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.AreEqual(-1, result);
         }
 
         [Test]
-        public void EmptyString_ReturnsTrue()
+        public void UniqueChar_EmptyString_ReturnsMinusOne()
         {
             // Arrange
             string input = "";
 
             // Act
-            bool result = Str.IsPalindrome(input);
+            int result = Str.UniqueChar(input);
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.AreEqual(-1, result);
         }
     }
 }
