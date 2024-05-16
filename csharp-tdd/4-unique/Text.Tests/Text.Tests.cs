437fc1cdd73bcbe70be59c200b8bce2ca2a4c8a2
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-15 19:12:35 -0500

    Updated

--- csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -1,73 +1,36 @@
+using System;
 using NUnit.Framework;
 using Text;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class StrTests
     {
-        [Test]
-        public void UniqueChar_FirstUniqueIsFirstChar_ReturnsZero()
-        {
-            // Arrange
-            string input = "helloworld";
-
-            // Act
-            int result = Str.UniqueChar(input);
-
-            // Assert
-            Assert.AreEqual(0, result);
-        }
-
-        [Test]
-        public void UniqueChar_FirstUniqueIsLastChar_ReturnsCorrectIndex()
-        {
-            // Arrange
-            string input = "helloholberton";
-
-            // Act
-            int result = Str.UniqueChar(input);
-
-            // Assert
-            Assert.AreEqual(8, result);
-        }
-
-        [Test]
-        public void UniqueChar_FirstUniqueInMiddle_ReturnsCorrectIndex()
-        {
-            // Arrange
-            string input = "aabbccddeef";
-
-            // Act
-            int result = Str.UniqueChar(input);
-
-            // Assert
-            Assert.AreEqual(10, result);
-        }
-
-        [Test]
-        public void UniqueChar_NoUniqueCharacters_ReturnsMinusOne()
-        {
-            // Arrange
-            string input = "hellohello";
-
-            // Act
-            int result = Str.UniqueChar(input);
-
-            // Assert
-            Assert.AreEqual(-1, result);
-        }
-
         [Test]
         public void UniqueChar_EmptyString_ReturnsMinusOne()
         {
             // Arrange
-            string input = "";
+            string s = "";
 
             // Act
-            int result = Str.UniqueChar(input);
+            int result = Str.UniqueChar(s);
 
             // Assert
-            Assert.AreEqual(-1, result);
+            Assert.That(result, Is.EqualTo(-1));
         }
-    }
-}
+
+        [Test]
+        public void UniqueChar_AllRepeatingCharacters_ReturnsMinusOne()
+        {
+            // Arrange
+            string s = "aaabbbccc";
+
+            // Act
+            int result = Str.UniqueChar(s);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(-1));
+        }
+
+        [Test]
