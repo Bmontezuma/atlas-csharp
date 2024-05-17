ca959d9bba73659c41c21440b9af5d27a844e8e2
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-17 08:43:52 +0000

    Initial commit

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VectorMath
+{
+    public static class VectorMath
+    {
+        /// <summary>
+        /// Calculates the magnitude of a given vector.
+        /// </summary>
+        /// <param name="vector">An array representing the vector components.</param>
+        /// <returns>The magnitude of the vector rounded to the nearest hundredth, or -1 if the vector is not 2D or 3D.</returns>
+        public static double Magnitude(double[] vector)
+        {
+            if (vector.Length == 2)
+            {
+                // 2D vector
+                return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
+            }
+            else if (vector.Length == 3)
+            {
+                // 3D vector
+                return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
+            }
+            else
+            {
+                // Invalid vector
+                return -1;
+            }
+        }
+    }
+}
+
+public class Class1
+{
+
+}
