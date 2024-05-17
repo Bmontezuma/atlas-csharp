3a34388985949e8f804eef04b08e25f3c433d928
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-17 09:52:22 +0000

    Updated 3-magnitude

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,36 +1,30 @@
 ﻿using System;
 
-namespace VectorMath
+public class VectorMath
 {
-    public static class VectorMath
+    public static double Magnitude(double[] vector)
     {
-        /// <summary>
-        /// Calculates the magnitude of a given vector.
-        /// </summary>
-        /// <param name="vector">An array representing the vector components.</param>
-        /// <returns>The magnitude of the vector rounded to the nearest hundredth, or -1 if the vector is not 2D or 3D.</returns>
-        public static double Magnitude(double[] vector)
+        if (vector.Length == 2)
         {
-            if (vector.Length == 2)
-            {
-                // 2D vector
-                return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2)), 2);
-            }
-            else if (vector.Length == 3)
-            {
-                // 3D vector
-                return Math.Round(Math.Sqrt(Math.Pow(vector[0], 2) + Math.Pow(vector[1], 2) + Math.Pow(vector[2], 2)), 2);
-            }
-            else
-            {
-                // Invalid vector
-                return -1;
-            }
+            // 2D vector: Calculate magnitude using Pythagorean theorem
+            double x = vector[0];
+            double y = vector[1];
+            double magnitude = Math.Sqrt(x * x + y * y);
+            return Math.Round(magnitude, 2);
+        }
+        else if (vector.Length == 3)
+        {
+            // 3D vector: Calculate magnitude using Euclidean norm
+            double x = vector[0];
+            double y = vector[1];
+            double z = vector[2];
+            double magnitude = Math.Sqrt(x * x + y * y + z * z);
+            return Math.Round(magnitude, 2);
+        }
+        else
+        {
+            // Invalid vector dimension
+            return -1;
         }
     }
-}
-
-public class Class1
-{
-
 }
