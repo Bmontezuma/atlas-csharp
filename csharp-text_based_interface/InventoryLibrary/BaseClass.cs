0154f056f2f5b281423a1c296a30b2e314aa552e
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-18 08:48:15 -0500

    Updated

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        /// <summary>
+        /// Unique identifier for the object.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Date and time when the object was created.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Date and time when the object was last updated.
+        /// </summary>
+        public DateTime DateUpdated { get; set; }
+
+        public BaseClass()
+        {
+            Id = Guid.NewGuid().ToString(); // Generate a new GUID for Id
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+    }
+}
