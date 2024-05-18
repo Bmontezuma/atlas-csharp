77e8a6b864f0c7515a2da60521a67f1c198813b6
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-18 08:17:02 -0500

    Updated

--- csharp-delegates_events/0-universal_health/main.cs
+++ csharp-delegates_events/0-universal_health/main.cs
@@ -1,5 +1,5 @@
 using System;
-using _0_universal_health;
+using _0_universal_health; 
 
 class Program
 {
@@ -7,8 +7,16 @@ class Program
     {
         Player player1 = new Player("Electric Mouse", 1000f);
         Player player2 = new Player("Electric Mouse");
+        Player player3 = new Player(maxHp: 972f);
+        Player player4 = new Player(default(string), 10f);
+        Player player5 = new Player();
+        Player player6 = new Player("Player", -50f);
 
         player1.PrintHealth();
         player2.PrintHealth();
+        player3.PrintHealth();
+        player4.PrintHealth();
+        player5.PrintHealth();
+        player6.PrintHealth();
     }
 }
