b405a277243f538f840554b927e6e727fad47333
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-20 18:46:47 -0500

    Updated

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,33 +1,27 @@
 ﻿﻿using System;
 
 /// <summary>
-/// Provides methods for vector mathematics.
+/// Provides mathematical methods for vector calculations.
 /// </summary>
-public class VectorMath
+public static class VectorMath
 {
     /// <summary>
-    /// Calculates the magnitude (length) of a vector.
+    /// Calculates and returns the magnitude of a given vector.
     /// </summary>
-    /// <param name="vector">The input vector, which can be 2D or 3D.</param>
-    /// <returns>The magnitude of the vector, rounded to the nearest hundredth.
-    /// If the input vector is not 2D or 3D, returns -1.</returns>
+    /// <param name="vector">The vector to calculate the magnitude for. Can be 2D or 3D.</param>
+    /// <returns>The magnitude of the vector rounded to the nearest hundredth, or -1 if the vector is not 2D or 3D.</returns>
     public static double Magnitude(double[] vector)
     {
-        // Check if the vector is 2D or 3D
-        if (vector.Length != 2 && vector.Length != 3)
+        if (vector.Length == 2 || vector.Length == 3)
         {
-            return -1;
+            double sumOfSquares = 0;
+            foreach (double component in vector)
+            {
+                sumOfSquares += component * component;
+            }
+            double magnitude = Math.Sqrt(sumOfSquares);
+            return Math.Round(magnitude, 2);
         }
-
-        double sum = 0;
-
-        // Calculate the sum of the squares of the vector components
-        for (int x = 0; x < vector.Length; x++)
-        {
-            sum += vector[x] * vector[x];
-        }
-
-        // Calculate and round the length of the vector
-        return Math.Round(Math.Sqrt(sum), 2);
+        return -1;
     }
 }
