770bf8b3437b89229311b2bf6ef5dd450944a2b0
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-21 18:20:58 -0500

    Updated

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,66 +1,43 @@
 ﻿﻿using System;
 
 /// <summary>
-/// Class representing a player with health attributes.
+/// Represents a player entity with health properties.
 /// </summary>
 public class Player
 {
-    private string name;
-    private float maxHp;
-    private float hp;
-
     /// <summary>
     /// Gets the name of the player.
     /// </summary>
-    public string Name
-    {
-        get { return name; }
-        private set { name = value; }
-    }
+    public string Name { get; private set; }
 
     /// <summary>
     /// Gets the maximum health points of the player.
     /// </summary>
-    public float MaxHp
+    public float MaxHp { get; private set; }
+
+    /// <summary>
+    /// Gets the current health points of the player.
+    /// </summary>
+    public float Hp { get; private set; }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="Player"/> class with the specified name and maximum health points.
+    /// </summary>
+    /// <param name="name">The name of the player.</param>
+    /// <param name="maxHp">The maximum health points of the player.</param>
+    public Player(string name = "Player", float maxHp = 100f)
     {
-        get { return maxHp; }
-        private set 
-        { 
-            if (value <= 0f)
-            {
-                Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
-                maxHp = 100f;
-            }
-            else
-            {
-                maxHp = value;
-            }
+        Name = name;
+        MaxHp = maxHp > 0 ? maxHp : 100f;
+        Hp = MaxHp;
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
     }
 
     /// <summary>
-    /// Gets or sets the current health points of the player.
-    /// </summary>
-    public float Hp
-    {
-        get { return hp; }
-        private set { hp = value; }
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="Player"/> class with a specified name and maximum health points.
-    /// </summary>
-    /// <param name="name">The name of the player. Default is "Player".</param>
-    /// <param name="maxHp">The maximum health points of the player. Default is 100f.</param>
-    public Player(string name = "Player", float maxHp = 100f)
-    {
-        Name = name;
-        MaxHp = maxHp;
-        Hp = MaxHp;
-    }
-
-    /// <summary>
-    /// Prints the player's health status.
+    /// Prints the health status of the player.
     /// </summary>
     public void PrintHealth()
     {
