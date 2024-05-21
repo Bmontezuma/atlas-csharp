fff748a961c3077f2f5b5235192ff1e8e2b9ed16
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-21 18:38:10 -0500

    Updated

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -1,62 +1,28 @@
 ﻿﻿using System;
 
-/// <summary>
-/// Represents a player entity with health properties.
-/// </summary>
-public class Player
+class Player
 {
-    private string _name;
-    private float _maxHp;
-    private float _hp;
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
 
-    /// <summary>
-    /// Gets or sets the name of the player.
-    /// </summary>
-    public string Name
-    {
-        get { return _name; }
-        private set { _name = value; }
-    }
-
-    /// <summary>
-    /// Gets or sets the maximum health points of the player.
-    /// </summary>
-    public float MaxHp
-    {
-        get { return _maxHp; }
-        private set { _maxHp = value > 0 ? value : 100f; }
-    }
-
-    /// <summary>
-    /// Gets the current health points of the player.
-    /// </summary>
-    public float Hp
-    {
-        get { return _hp; }
-        private set { _hp = value; }
-    }
-
-    /// <summary>
-    /// Creates a new instance of the <see cref="Player"/> class with the specified name and maximum health points.
-    /// </summary>
-    /// <param name="name">The name of the player.</param>
-    /// <param name="maxHp">The maximum health points of the player.</param>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        Name = name;
-        MaxHp = maxHp;
-        Hp = MaxHp;
         if (maxHp <= 0)
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
         }
-    }
+        else
+        {
+            this.maxHp = maxHp;
+        }
 
-    /// <summary>
-    /// Prints the health status of the player.
-    /// </summary>
+        this.hp = this.maxHp;
+        this.name = name;
+    }
+    
     public void PrintHealth()
     {
-        Console.WriteLine($"{Name} has {Hp} / {MaxHp} health");
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
     }
 }
