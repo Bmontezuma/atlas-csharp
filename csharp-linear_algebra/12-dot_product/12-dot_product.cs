26ae58e8dc6345d5979647bff3351ed92dcfbec1
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-05-22 20:23:10 -0500

    Updated

--- csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,45 +1,98 @@
-﻿using System.Linq;
-using System;
+﻿using System;
+using System.Linq;
+
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(1) != matrix2.GetLength(0))
+            return new double[,] { { -1 } };
+
+        int rowSize = matrix1.GetLength(0);
+        int colSize = matrix2.GetLength(1);
+        var result = new double[rowSize, colSize];
+
+        for (int row = 0; row < rowSize; row++)
+        {
+            for (int col = 0; col < colSize; col++)
+            {
+                result[row, col] = VectorMath.DotProduct(GetRow(matrix1, (uint)row), GetColumn(matrix2, (uint)col));
+            }
+        }
+
+        return result;
+    }
+
+    public static double[] GetRow(double[,] matrix, uint row)
+    {
+        int size = matrix.GetLength(1);
+        double[] r = new double[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            r[i] = matrix[row, i];
+        }
+
+        return r;
+    }
+
+    public static double[] GetColumn(double[,] matrix, uint column)
+    {
+        int size = matrix.GetLength(0);
+        var col = new double[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            col[i] = matrix[i, column];
+        }
+
+        return col;
+    }
+}
 
 class VectorMath
 {
-
-public static double DotProduct(double[] vector1, double[] vector2) {
-    int size = 0;
-    if ((size = vector1.Count()) != vector2.Count() || !(IsVector(vector1) || IsVector(vector2)))
-        return -1;
-
-    double[] dot = new double[size];
-
-    for (int i = 0; i < size; i++) {
-        dot[i] = vector1[i] * vector2[i];
-    }
-
-    return dot.Sum();
-}
-    public static double Magnitude(double[] vector)
+    public static double DotProduct(double[] vector1, double[] vector2)
     {
+        int size = vector1.Length;
 
-        if (vector.Count() < 2 || vector.Count() > 3)
+        if (size != vector2.Length || !(IsVector(vector1) && IsVector(vector2)))
             return -1;
 
-        for (int i = 0; i < vector.Count(); i++)
-        {
-            vector[i] *= vector[i];
-        }
-        return Math.Round(Math.Sqrt(Math.Abs(vector.Sum())), 1);
+        double[] dot = new double[size];
 
+        for (int i = 0; i < size; i++)
+        {
+            dot[i] = vector1[i] * vector2[i];
+        }
+
+        return dot.Sum();
+    }
+
+    public static double Magnitude(double[] vector)
+    {
+        int size = vector.Length;
+
+        if (size < 2 || size > 3)
+            return -1;
+
+        double magnitude = 0;
+
+        for (int i = 0; i < size; i++)
+        {
+            magnitude += vector[i] * vector[i];
+        }
+
+        return Math.Round(Math.Sqrt(magnitude), 1);
     }
 
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        int size;
-        double[] sum;
+        int size = vector1.Length;
+        double[] sum = new double[size];
 
-        if ((size = vector1.Count()) != vector2.Count() || !IsVector(vector1) || !IsVector(vector2))
+        if (size != vector2.Length || !IsVector(vector1) || !IsVector(vector2))
             return new double[] { -1 };
-
-        sum = new double[size];
 
         for (int i = 0; i < size; i++)
         {
@@ -47,14 +100,12 @@ public static double DotProduct(double[] vector1, double[] vector2) {
         }
 
         return sum;
-
     }
 
     static bool IsVector(double[] vec)
     {
-        if (vec.Count() == 2 || vec.Count() == 3)
-            return true;
-        return false;
+        int size = vec.Length;
+        return size == 2 || size == 3;
     }
 
     public static double[] Multiply(double[] vector, double scalar)
@@ -62,10 +113,11 @@ public static double DotProduct(double[] vector1, double[] vector2) {
         if (!IsVector(vector))
             return new double[] { -1 };
 
-        for (int i = 0; i < vector.Count(); i++)
+        for (int i = 0; i < vector.Length; i++)
+        {
             vector[i] *= scalar;
+        }
 
         return vector;
     }
 }
-
--- csharp-linear_algebra/12-dot_product/main.cs
+++ csharp-linear_algebra/12-dot_product/main.cs
@@ -1,12 +1,27 @@
 using System;
 
-class Program
+namespace _18_matrix_matrix_mul
 {
-    static void Main(string[] args)
+    class Program
     {
-        var s = VectorMath.DotProduct(new double[]{-4, 0, 10}, new double[]{3, 7, -9});
-        Console.WriteLine("{0}", s);
+        static void Main(string[] args)
+        {
+            var a = new double[,] { { 1, 2 }, { 4, 5 }, { 7, 8 } };
+            var b = new double[,] {{2,3},{4,5}};
+
+            Console.WriteLine("Rows {0}, Columns {1}", a.GetLength(0), a.GetLength(1));
+
+            var matrix = MatrixMath.Multiply(a, b);
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write("Row: ");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"|{matrix[i, j]}|");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
-
-
